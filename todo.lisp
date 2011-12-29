58a6fb3565c14c961a6f4fece0978392ee20706f
Ryos Suzuki <ryos@sinby.com>
2011-12-29 17:09:59 +0900

    dekita kiga suru.

--- todo.lisp
+++ todo.lisp
@@ -13,106 +13,111 @@
 				       (incf sum (- e s)))
 		  sum)
 	    detail 
-	    (cl-ppcre:scan-to-strings "[0-9]+" estimate))))
+	    (let ((str-estimate (cl-ppcre:scan-to-strings "[0-9]+" estimate)))
+	      (if (null str-estimate) 0
+		(read-from-string str-estimate))))))
 
-(defun up-stair (layer-list tmp-list t-list)
-  (let* ((parents (pop layer-list))
-	 (last-parent (pop parents))
-	 (last-item (car (last last-parent)))
-	 last-child-last-parent)
-    (if (listp last-item) 
-      (setf last-child-last-parent last-item
-	    last-parent (delete-if #'listp last-parent)))
-
-    (format t "upstair <~a> <~a> <~a> <~a>~%" layer-list parents last-parent last-child-last-parent)
-
-    (format t "upstair last-parent <~a> ~%" (append nil (reverse tmp-list)))
-
-    (setf last-parent
-	  (append last-parent (append last-child-last-parent (reverse tmp-list))))
-    (format t "upstair last-parent <~a> ~%" last-parent)
-    (push last-parent parents)
-    (if t-list
-      (push t-list parents))
-    (values layer-list parents)))
-
-(setf layer-list nil)
-(setf tmp-list nil)
-(push '((top1 2)) layer-list)
-(push '(sub2-1 1) tmp-list)
-(push '(sub2-2 2) tmp-list)
-(push tmp-list layer-list)
-(setf tmp-list nil)
-(push '(sub3 3) tmp-list)
-(format t "~a ~a ~a~%" layer-list tmp-list nil)
-(multiple-value-bind (layer-list tmp-list)
-		     (up-stair layer-list tmp-list '(sub2-3 3)))
-(format t "xxx <~a> <~a> <~a>~%" layer-list tmp-list nil)
-(quit)
-
-(defun list-to-layer (alist) 
-  (let ((level 1) result tmp-list)
-    (mapcar (lambda (a-todo)
-	      (let ((t-level (car a-todo))
-		    (t-todo (cdr a-todo)))
-		(cond ((= level t-level)
-		       (push t-todo tmp-list))
-		      ((< level t-level)
-		       (progn
-			 (push tmp-list result)
-			 (setf tmp-list nil)
-			 (push t-todo tmp-list)))
-		      ((> level t-level)
-		       (multiple-value-bind (result tmp-list)
-		       	(up-stair result tmp-list t-level))))
-		(setf level t-level)
-	      (format t "~a [~a ~a] ~a ~a~%" level t-level t-todo result tmp-list)
-	      ))
-	    alist)
-    (nreverse result)))
+;----------------------------------------------------------------
+(defun sum-children (children) 
+  (reduce (lambda (sum child)
+	    (let* ((detail-estimate (car child))
+		   (new-children (cadr child))
+		   (detail (car detail-estimate))
+		   (estimate (cadr detail-estimate)))
+	      (incf sum 
+		    (if (null new-children)
+		      estimate
+		      (sum-children new-children)))))
+	  children
+	  :initial-value 0))
 
 (defun view-one (a-layer-list level)
-  (let ((detail (car a-layer-list))
-	(estimate (cadr a-layer-list)))
+  (let* ((detail-estimate (car a-layer-list))
+	 (children (cadr a-layer-list))
+	 (detail (car detail-estimate))
+	 (estimate (cadr detail-estimate)))
     (dotimes (i level nil) 
-      (format t "+"))
-    (format t " ~a" detail)
-    (if (listp estimate)
-      (progn
-	(format t "~%")
-	(view-layer estimate (+ level)))
-      (format t " ~a~%" estimate))))
+      (format t "+" ))
+
+    (format t " ~a ~a ~%" detail 
+	    (if (null children)
+	      estimate
+	      (sum-children children)))
+    (if (listp children)
+      (view-layer children (+ level 1)))))
 
 (defun view-layer (layer-list level)
   (dolist (item layer-list)
+    ;(format t "item [~a]~%" item);
     (view-one item level)))
 
+;----------------------------------------------------------------
+(defun view-debug-layer (level-layer-list)
+  (let ((level (car level-layer-list))
+	(layer-list (cdr level-layer-list)))
+    (view-layer layer-list level)))
+
+;----------------------------------------------------------------
+; '((level ((sub 2) (sub 3))))
+; ((3 ((sub3-2 2) ((sub4 3))) ((sub3-3 3) nil)) (1 ((main2 3) nil) ((main3 4) nil)))
+(defun have-item-same-level (layer-list level)
+  (if (null layer-list) nil
+    (let ((layer-list-level (caar layer-list)))
+      (if (= layer-list-level level)
+	(cdr (pop layer-list))))))
+
+(defun layer-list-add-a-todo (layer-list level tmp-list a-todo)
+  (let ((t-level (car a-todo))
+	(t-todo (cdr a-todo)))
+    (cond ((= t-level level)
+	   (push (list t-todo nil) tmp-list))
+	  ((= t-level (- level 1))
+	   (setf tmp-list
+		 (let ((new-todo (list t-todo tmp-list))
+		       (upper-list (have-item-same-level layer-list t-level)))
+		   (push new-todo upper-list)
+		   upper-list)))
+	  ((> t-level level)
+	   (progn
+	     (if tmp-list
+	       (push
+		 (cons level tmp-list)
+		 layer-list))
+	     (setf tmp-list (list (list t-todo nil)))))
+	  (t (error "format error")))
+
+    (format t "----------------------~%");
+    (format t "<~a> ~a ~a <~a>~%" layer-list t-level level tmp-list)
+    (view-debug-layer layer-list)
+    (format t "----------------------~%");
+
+    (values layer-list t-level tmp-list)))
+
+(defun list-to-layer (a-reverse-list)
+  (let (layer-list (level 1) tmp-list)
+    (dolist (item a-reverse-list layer-list)
+      (multiple-value-bind (v-layer-list v-level v-tmp-list)
+	(layer-list-add-a-todo layer-list level tmp-list item)
+	(setf layer-list v-layer-list
+	      level v-level
+	      tmp-list v-tmp-list))
+
+      (format t "list-to-layer <~a> ~a <~a>~%" layer-list level tmp-list))
+    (format t "last <~a> <~a>~%" layer-list tmp-list)
+    (view-layer tmp-list 1)
+    ))
+;----------------------------------------------------------------
 (setf file-name "todo.txt")
 (setf layer-data
       (list-to-layer
 	(with-open-file (stream file-name)
 	  (do ((line (read-line stream nil)
-		     (read-line stream nil)) result a-todo)
-	    ((null line) (nreverse result))
+		     (read-line stream nil)) reverse-result a-todo)
+	    ((null line) reverse-result)
 	    (setf a-todo (a-todo-to-lisp line))
 	    ;(print a-todo)
-	    (push a-todo result)))))
+	    (push a-todo reverse-result)
+	    reverse-result))))
 
-(format t "~a~%" layer-data 1)
-(format t "~a~%" (view-layer layer-data 1))
-
-#|
-(setf l (read-line))
-(format t "~a~%" l)
-(format t "~a~%"
-	(cl-ppcre:register-groups-bind 
-	  (section detail estimate)
-	  ("(\\++) ([^,]*), *([0-9]*)[hH]$" l)
-	  (list section detail estimate)))
-
-(format t "~a~%" 
-	(eval 
-	  (with-input-from-string
-	    (in-str l)
-	    (read in-str))))
-|#
+(view-layer layer-data 1)
+;----------------------------------------------------------------
